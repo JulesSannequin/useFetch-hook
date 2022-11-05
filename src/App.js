bfa5c374fbccfd713ee125d55a0dda02df43fc7e
Jules Sannequin <jules.sannequin@gmail.com>
2022-11-05 12:51:41 +0100

    create the useFetch hook with the loading and the error useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useFetch from "./useFetch";
 
 function App() {
+  const { data, isLoading, error } = useFetch(
+    "https://v2.jokeapi.dev/joke/any"
+  );
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) console.log(error);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {data?.setup} : {data?.delivery}
+      </h1>
     </div>
   );
 }
